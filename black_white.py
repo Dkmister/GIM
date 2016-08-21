e8b6ab58114cc9ef668c49ec129978654647d7bc
Vilmar Neto <vilmar.neto@gmail.com>
2016-08-20 23:39:01 -0300

    Strange things

--- black_white.py
+++ black_white.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 #===============================
 #   Given a image, return a Image BW
-#   
+#   This is just a BS
 #===============================
 def black_white(a_image):
     new_image = Image.new("L",a_image.size)
@@ -12,11 +12,20 @@ def black_white(a_image):
     
     for i in xrange(w):
         for j in xrange(h):
-            rgb = im.getpixel((i,j))
+            rgb = a_image.getpixel((i,j))
             l = 0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]
-            
+            print l
+            rgb_new = new_image.getpixel((i,j))
+            # rgb_new[0] = l
+            # rgb_new[1] = l
+            # rgb_new[2] = l
+            # WHY THE FUCK CAN'T I DO THAT ?
+            # WHAT'S SO FUCKING WRONG IN PROGRAMMING?
+            # FUCK THIS SHIT!!!!
+    return new_image
             
 
 im = Image.open("young-stalin.jpeg")
-black_white(im)
+nw_im = black_white(im)
+nw_im.show()
 

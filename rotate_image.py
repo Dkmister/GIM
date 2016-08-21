3ae290ffd660456d26a72c1e033b4e634d609064
Vilmar Neto <vilmar.neto@gmail.com>
2016-08-20 22:30:59 -0300

    Idea of rotating a image 180 in a function

--- rotate_image.py
+++ rotate_image.py
@@ -1,21 +1,20 @@
 from PIL import Image
 
-
-#def rotate_image(a_image):
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#===============================
+#   Given a image, return a Image matrix with
+#   Inverted values of the original one
+#===============================
+def rotate_image(a_image):
+    zero_triples = (0,0,0)
+    xy = a_image.size
+    w = xy[0]
+    h = xy[1]
+    # new_image = zero_triples[w][h]
+    for i in xrange(0,w-1):
+        for j in xrange(0,h-1):
+            rgb = im.getpixel((i,j))
+            # new_image[w-i-1][h-j-1] += rgb[i][j]
 
 im = Image.open("young-stalin.jpeg")
-print im
+rotate_image(im)
+

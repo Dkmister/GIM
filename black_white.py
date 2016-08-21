6394a497f2e06ae63fcb7634c61f04fbffa11f4b
Vilmar Neto <vilmar.neto@gmail.com>
2016-08-21 01:57:16 -0300

    Black and White & Filter concluded
    Finished and working!

--- black_white.py
+++ black_white.py
@@ -5,27 +5,19 @@ from PIL import Image
 #   This is just a BS
 #===============================
 def black_white(a_image):
-    new_image = Image.new("L",a_image.size)
-    xy = a_image.size
-    w = xy[0]
-    h = xy[1]
+    new_image = Image.new("RGB",a_image.size,"black")
+    pixels = new_image.load() #create the pixel map
     
-    for i in xrange(w):
-        for j in xrange(h):
+    for i in range(a_image.size[0]):
+        for j in range(a_image.size[1]):
             rgb = a_image.getpixel((i,j))
             l = 0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]
-            print l
-            rgb_new = new_image.getpixel((i,j))
-            # rgb_new[0] = l
-            # rgb_new[1] = l
-            # rgb_new[2] = l
-            # WHY THE FUCK CAN'T I DO THAT ?
-            # WHAT'S SO FUCKING WRONG IN PROGRAMMING?
-            # FUCK THIS SHIT!!!!
+            pixels[i,j] = (int(l),int(l),int(l))
     return new_image
+    
             
 
-im = Image.open("young-stalin.jpeg")
+im = Image.open("batman.jpeg")
 nw_im = black_white(im)
 nw_im.show()
 
---
+++ filter_perfect_illusion.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+#===============================
+#   Given a image, return a Image Filtered
+#   Using l = 0.299*r+0.587*g+0.114*b
+#   Then aplying, return a filtered image
+#===============================
+def filter_perfect_illusion(a_image):
+    new_image = Image.new("RGB",a_image.size,"black")
+    pixels = new_image.load()
+    xy = a_image.size
+    w = xy[0]
+    h = xy[1]
+    
+    for i in xrange(w):
+        for j in xrange(h):
+            rgb = a_image.getpixel((i,j))
+            l = 0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]
+            pixels[i,j] = (i,j,int(l))
+    return new_image
+            
+
+im = Image.open("batman.jpeg")
+nw_im = filter_perfect_illusion(im)
+nw_im.show()
+

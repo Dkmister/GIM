64b6437c4b4650f389709f825841b5ccb29c36ba
Vilmar Neto <vilmar.neto@gmail.com>
2016-08-28 03:46:11 -0300

    Fixed some things

--- menu.py
+++ menu.py
@@ -1,19 +1,22 @@
-import easygui_qt as easy 
+import easygui_qt as easy
+from easygui import *
+import time
 from black_white import *
 from quantitization import *
 from rotate_image_180 import *
 from rotate_image_90 import *
 
-def init_image():
+
+
+
+def menu_choices():
     link_im = easy.get_file_names()
-    return link_im[0] 
-
-
-def menu_choices(link_im):
-    choices = ["Blank and White","Quantum of Solace","Spin Me Round 180","Like a record 90,baby!"]
-    old_im = Image.open(link_im)
-    old_im.show()
-    #here reply = easy.get_choice("Choose an option:",choices=choices)
+    old_im = Image.open(link_im[0])
+    msgbox("Welcome to bizarre program!")
+    msg ="What is your choice?"
+    title = "Image Operations"
+    choices = ["BW", "Rotate 180", "Rotate 90", "Quantum"]
+    choice = choicebox(msg, title, choices)
     
 
 
@@ -22,8 +25,8 @@ def menu_choices(link_im):
 
 
 def main():
-    link_im = init_image()
-    menu_choices(link_im)
+    
+    menu_choices()
 
 
 

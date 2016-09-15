f04da1a3b84349c99db76942f231a3d6c08d5ab8
Vilmar Neto <vilmar.neto@gmail.com>
2016-09-15 00:05:27 -0300

    Histogram function complete

--- histogram_array.py
+++ histogram_array.py
@@ -17,13 +17,38 @@ def histogram_list(a_image):
                 hist_lst[rgb[0]] += 1
 
 
-
+		
+	
     return hist_lst
-#-------------------------------
+
+def show_bw_histogram(a_image):
+    xy = a_image.size
+    im_siz = xy[0]*xy[1]
+    alpha_b = []
+    for i in xrange(0,256):
+        alpha_b.append(i)
+    pos = np.arange(len(alpha_b))
+    width = 1.0
+
+    bw_im = black_white(a_image)
+    lst = histogram_list(bw_im)
+
+    ax = plt.axes()
+    ax.set_xticks(pos + (width / 2))
+    ax.set_xticklabels(alpha_b)
+
+    plt.bar(pos,lst,width,color='black')
+    plt.show()
+
+   
+    
+
+#-----------------
 # Tests following the image
 #-------------------------------
-# im = Image.open("young-stalin.jpeg")
-# nw_im = black_white(im)
-# lst = histogram_list(nw_im)
-# print lst , len(lst)
-                
+im = Image.open("young-stalin.jpeg")
+#nw_im = black_white(im)
+#lst = histogram_list(nw_im)
+#print lst , len(lst)
+show_bw_histogram(im)
+            
